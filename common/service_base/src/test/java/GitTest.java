dd940693dfb9b07d3af4bfc903c58e2173bb39b7
xiazhaoguo <xiazhaoguomail@qq.com>
2021-12-17 13:30:37 +0800

    hotfix commit

--- common/service_base/src/test/java/GitTest.java
+++ common/service_base/src/test/java/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
     public static void main(String[] args) {
 
         System.out.println("Git 1");
-        System.out.println("Git hotfix");
+        System.out.println("Git hotfix 1");
+        System.out.println("Git hotfix 2");
     }
 }
